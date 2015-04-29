426b11923fed3adf6cfa0809815839b53abc4a16
John Marshall <john@themillhousegroup.com>
2015-04-30 09:14:53 +1000

    Tests. Again, monolithic. Needs tidying.

---
+++ src/test/scala/com/themillhousegroup/arallon/TimeSpec.scala
@@ -0,0 +1,127 @@
+package com.themillhousegroup.arallon
+
+import org.specs2.mutable.Specification
+import org.specs2.mock._
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import org.joda.time.DateTime
+
+class TimeSpec extends Specification {
+
+  "Strongly-typed time" should {
+    "allow construction of current time in a zone" in {
+      val hereNow: TimeInZone[_ <: TimeZone] = TimeInZone.now
+
+      println(s"hereNow is a $hereNow")
+      hereNow must not beNull
+    }
+
+    "allow construction of current time in an explicit zone" in {
+      val gmtNow: TimeInZone[_ <: TimeZone] = TimeInZone.now("GMT")
+
+      println(s"gmtNow is a $gmtNow")
+      gmtNow must not beNull
+    }
+
+    "allow construction of a time from millis, which must be in UTC" in {
+      val millis = 1430222400000L
+      val someUTCTime: TimeInZone[UTC] = TimeInZone.fromUTCMillis(millis)
+
+      println(s"someUTCTime is a $someUTCTime")
+      someUTCTime must not beNull
+
+      val melTime = someUTCTime.map[Melbourne]
+      println(s"melTime: $melTime")
+
+      melTime.utcMillis must beEqualTo(millis)
+    }
+
+    "allow construction of a UTC time representing 'now'" in {
+      val nowUTCTime: TimeInZone[UTC] = TimeInZone.nowUTC
+
+      println(s"nowUTCTime is $nowUTCTime")
+
+      nowUTCTime must not beNull
+
+      val millis = nowUTCTime.utcMillis
+
+      val newUTCTime: TimeInZone[UTC] = TimeInZone.fromUTCMillis(millis)
+
+      newUTCTime must beEqualTo(nowUTCTime)
+    }
+
+    "allow construction of current time in a zone purely by type signature" in {
+      val paris = TimeInZone[Paris]
+      val melbourne = TimeInZone[Melbourne]
+
+      //			println(s"paris is a $paris")
+      //			println(s"mel is a $melbourne")
+
+      paris must not be equalTo(melbourne)
+
+      paris.timezone must not be equalTo(melbourne.timezone)
+    }
+
+    "permit comparison of current times in a zone" in {
+      val now = new DateTime()
+      val paris = TimeInZone[Paris](now)
+      val melbourne = TimeInZone[Melbourne](now)
+      val parisInMel = paris.map[Melbourne]
+
+      println(s"paris is a   $paris")
+      println(s"mel is a $melbourne")
+      println(s"pIM is a $parisInMel")
+
+      paris must not be equalTo(melbourne)
+
+      paris.timezone must not be equalTo(melbourne.timezone)
+
+      paris.utc must not be equalTo(melbourne.utc)
+    }
+
+    "Not permit silly programming errors" in {
+      val paris = TimeInZone[Paris]
+      val melbourne = TimeInZone[Melbourne]
+
+      def somethingThatWorksInMelbourne(mel: TimeInZone[Melbourne]): Boolean = {
+        true
+      }
+
+      def somethingThatWorksAnywhere(any: TimeInZone[_]): Boolean = {
+        true
+      }
+
+      somethingThatWorksInMelbourne(melbourne) must beTrue
+
+      // Won't compile. By design. Try it! :-)
+      // somethingThatWorksInMelbourne(paris) must beTrue
+
+      somethingThatWorksAnywhere(melbourne) must beTrue
+      somethingThatWorksAnywhere(paris) must beTrue
+
+    }
+
+    "allow me to map from one timezone to another" in {
+      val paris = TimeInZone[Paris]
+
+      val melbourne = paris.map[Melbourne]
+      paris must not be equalTo(melbourne)
+
+      paris.timezone must not be equalTo(melbourne.timezone)
+
+      paris.utc must be equalTo (melbourne.utc)
+    }
+
+    "allow me to transform a time within a timezone" in {
+      val paris = TimeInZone[Paris]
+
+      val laterThatDay = paris.transform(_.plusHours(3))
+      paris must not be equalTo(laterThatDay)
+
+      paris.timezone must beEqualTo(laterThatDay.timezone)
+
+      paris.utc must not be equalTo(laterThatDay.utc)
+    }
+  }
+}
