f59f62d7e5745dfc12af647e62416e01bb4fc941
John Marshall <john@themillhousegroup.com>
2015-05-06 20:57:35 +1000

    Test case to confirm autogeneration of TimeZone scala

--- src/test/scala/com/themillhousegroup/arallon/util/TZBuilderSpec.scala
+++ src/test/scala/com/themillhousegroup/arallon/util/TZBuilderSpec.scala
@@ -1,5 +1,21 @@
 package com.themillhousegroup.arallon.util
 
-class TZBuilderSpec {
+import org.specs2.mutable.Specification
+
+class TZBuilderSpec extends Specification {
+
+  "TZBuilder" should {
+
+    "have the most up-to-date list of timezones possible" in {
+      TZBuilder.allTimeZoneStrings must haveSize(619) // If this fails, we know that we need to rebuild...
+    }
+
+    "be able to create a sequence of TimeZones" in {
+
+      TZBuilder.rebuildTimeZones() must not beEmpty
+
+      TZBuilder.rebuildTimeZones() must haveSize(547) // If this fails, we know that we need to rebuild...
+    }
+  }
 
 }
