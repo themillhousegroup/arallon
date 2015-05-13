b961d79a648fe4291b1628b3bece92b7cc867a53
John Marshall <john@themillhousegroup.com>
2015-05-13 21:23:35 +1000

    Tests for TimeZone construction and comparison

---
+++ src/test/scala/com/themillhousegroup/arallon/TimeZoneSpec.scala
@@ -0,0 +1,35 @@
+package com.themillhousegroup.arallon
+
+import org.specs2.mutable.Specification
+import org.joda.time.DateTime
+import com.typesafe.scalalogging.slf4j._
+import com.themillhousegroup.arallon.zones._
+
+class TimeZoneSpec extends Specification with LazyLogging {
+
+  "TimeZones" should {
+    "be unequal if referring to a different zone" in {
+      val paris = new Paris()
+      val london = new London()
+
+      paris must not be equalTo(london)
+    }
+
+    "be equal if referring to the same zone" in {
+      val p1 = new Paris()
+      val p2 = new Paris()
+
+      p1 must beEqualTo(p2)
+    }
+
+    "be equal even if constructed different ways" in {
+      val p1 = new Paris()
+      val p2 = TimeZone.apply("Europe/Paris")
+      val p3 = TimeZone.apply("Europe/Paris")
+
+      p1 must beEqualTo(p2)
+      p2 must beEqualTo(p3)
+    }
+
+  }
+}
