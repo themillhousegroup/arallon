fe7cf8015a308ff1c244496163693a304da0ce1e
John Marshall <john@themillhousegroup.com>
2015-05-26 09:09:57 +1000

    Remove TypeTag-based reflection. Works poorly under Scala 2.10

--- src/main/scala/com/themillhousegroup/arallon/util/ReflectionHelper.scala
+++ src/main/scala/com/themillhousegroup/arallon/util/ReflectionHelper.scala
@@ -14,14 +14,4 @@ object ReflectionHelper {
   def construct[T](c: ClassTag[T], args: List[Object]) = {
     constructor(c).newInstance(args.toArray: _*).asInstanceOf[T]
   }
-
-  private def constructor[_](m: RuntimeMirror, t: Type): Constructor[_] = {
-    val c = m.runtimeClass(t.typeSymbol.asClass)
-    c.getConstructors()(0)
-  }
-
-  def construct[T](t: Type, args: List[Object]) = {
-    constructor(m, t).newInstance(args.toArray: _*).asInstanceOf[T]
-  }
-
 }
