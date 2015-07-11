2ab39619c413df4c2c0b91ac7ed1c5d3d9a2c9e2
John Marshall <john@themillhousegroup.com>
2015-07-12 08:47:15 +1000

    Test toString of normal TimeZones

--- src/test/scala/com/themillhousegroup/arallon/TimeZoneSpec.scala
+++ src/test/scala/com/themillhousegroup/arallon/TimeZoneSpec.scala
@@ -7,27 +7,26 @@ import com.themillhousegroup.arallon.zones._
 
 class TimeZoneSpec extends Specification with LazyLogging {
 
+  val paris = new Paris()
+
   "TimeZones" should {
     "be unequal if referring to a different zone" in {
-      val paris = new Paris()
       val london = new London()
 
       paris must not be equalTo(london)
     }
 
     "be equal if referring to the same zone" in {
-      val p1 = new Paris()
       val p2 = new Paris()
 
-      p1 must beEqualTo(p2)
+      paris must beEqualTo(p2)
     }
 
     "be equal even if constructed different ways" in {
-      val p1 = new Paris()
       val p2 = TimeZone.apply("Europe/Paris")
       val p3 = TimeZone.apply("Europe/Paris")
 
-      p1 must beEqualTo(p2)
+      paris must beEqualTo(p2)
       p2 must beEqualTo(p3)
     }
 
@@ -36,6 +35,10 @@ class TimeZoneSpec extends Specification with LazyLogging {
   "TimeZone toString" should {
     "render UTC as simply 'UTC'" in {
       TimeZone.UTC.toString must beEqualTo("UTC")
+    }
+
+    "render a normal timezone by its trailing element" in {
+      paris.toString must beEqualTo("Paris")
     }
   }
 }
