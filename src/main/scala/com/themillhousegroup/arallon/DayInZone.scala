a29a9122538599dec147e2c9fd644ebbbfbb6997
John Marshall <john@themillhousegroup.com>
2015-07-15 14:47:58 +1000

    Simpler toString for a DayInZone - just want to know the TZ and the day in question

--- src/main/scala/com/themillhousegroup/arallon/DayInZone.scala
+++ src/main/scala/com/themillhousegroup/arallon/DayInZone.scala
@@ -2,6 +2,7 @@ package com.themillhousegroup.arallon
 
 import org.joda.time._
 import com.themillhousegroup.arallon.util.ReflectionHelper
+import org.joda.time.format.{ DateTimeFormat, DateTimeFormatter }
 import com.themillhousegroup.arallon.traits._
 import scala.reflect._
 import scala.reflect.ClassTag
@@ -11,6 +12,10 @@ import scala.reflect.ClassTag
  * Initially the way to get one is to pass a TimeInZone[TZ] that is somewhere inside this day.
  */
 object DayInZone {
+
+  val jodaYMDFormat = "yyyy-MM-dd"
+  val ymdFormatter: DateTimeFormatter = DateTimeFormat.forPattern(jodaYMDFormat)
+
   def expand[TZ <: TimeZone](t: TimeInZone[TZ]): DayInZone[TZ] = {
     val start = t.local.withTime(0, 0, 0, 0)
     val end = t.local.withTime(23, 59, 59, 999)
@@ -19,6 +24,10 @@ object DayInZone {
 }
 
 class DayInZone[TZ <: TimeZone](override val timezone: TZ,
-  override val start: DateTime,
-  override val end: DateTime) extends TimeSpanInZone[TZ](timezone, start, end)
+    override val start: DateTime,
+    override val end: DateTime) extends TimeSpanInZone[TZ](timezone, start, end) {
+
+  override val toString: String = s"DayInZone[${timezone}] : ${start.toString(DayInZone.ymdFormatter)}"
+
+}
 
